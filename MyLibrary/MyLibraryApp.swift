1606d422257bc408992267052d9c987574fec0d6
dustom <dusek.tomas@icloud.com>
2025-02-28 11:43:19 +0100

    Initial Commit

---
+++ MyLibrary/MyLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLibraryApp.swift
+//  MyLibrary
+//
+//  Created by Tomáš Dušek on 28.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
